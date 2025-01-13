b2c64124e4b8d4ea7bad87e8924d0c0cc5dfc8a0
Anass-Dr <anasdrissi77@gmail.com>
2025-01-13 11:59:16 +0100

    feat: add FavoritesModule to AppModule and make repositories global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirestoreModule } from './firestore/firestore.module';
@@ -6,15 +6,19 @@ import { ConfigModule } from '@nestjs/config';
 import { UserRepository } from './repositories/user.repository';
 import { PharmacyRepository } from './repositories/pharmacy.repository';
 import { ReviewRepository } from './repositories/review.repository';
+import { FavoritesModule } from './favorites/favorites.module';
 
+@Global()
 @Module({
   imports: [
     FirestoreModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    FavoritesModule,
   ],
   controllers: [AppController],
   providers: [AppService, UserRepository, PharmacyRepository, ReviewRepository],
+  exports: [UserRepository, PharmacyRepository, ReviewRepository],
 })
 export class AppModule {}
